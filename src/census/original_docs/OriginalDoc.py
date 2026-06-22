14fad9656179499fb5c5decbd120a411454952d0
nuuuwan <nuuuwan@gmail.com>
2026-06-22 16:09:04 +0530

    feat: add OriginalDocReadmeMixin for enhanced documentation handling

--- src/census/original_docs/OriginalDoc.py
+++ src/census/original_docs/OriginalDoc.py
@@ -2,11 +2,14 @@ from census.original_docs.OriginalDocBase import OriginalDocBase
 from census.original_docs.OriginalDocDataMixin import OriginalDocDataMixin
 from census.original_docs.OriginalDocLoaderMixin import OriginalDocLoaderMixin
 from census.original_docs.OriginalDocPDFMixin import OriginalDocPDFMixin
-from census.original_docs.OriginalDocRawDataMixin import \
-    OriginalDocRawDataMixin
+from census.original_docs.OriginalDocRawDataMixin import (
+    OriginalDocRawDataMixin,
+)
+from census.original_docs.OriginalDocReadmeMixin import OriginalDocReadmeMixin
 from census.original_docs.OriginalDocScrapeMixin import OriginalDocScrapeMixin
-from census.original_docs.OriginalDocValidateMixin import \
-    OriginalDocValidateMixin
+from census.original_docs.OriginalDocValidateMixin import (
+    OriginalDocValidateMixin,
+)
 from utils_future import Log
 
 log = Log("OriginalDoc")
@@ -14,12 +17,15 @@ log = Log("OriginalDoc")
 
 class OriginalDoc(
     OriginalDocBase,
+    OriginalDocLoaderMixin,
+    #
     OriginalDocScrapeMixin,
     OriginalDocPDFMixin,
     OriginalDocRawDataMixin,
     OriginalDocDataMixin,
+    OriginalDocReadmeMixin,
     OriginalDocValidateMixin,
-    OriginalDocLoaderMixin,
+    #
 ):
 
     pass
