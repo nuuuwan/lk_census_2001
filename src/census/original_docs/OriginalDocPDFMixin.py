cf54a0b722101023285c8396c2450e9db8df86d3
nuuuwan <nuuuwan@gmail.com>
2026-06-22 12:02:24 +0530

    copy pdfs

--- src/census/original_docs/OriginalDocPDFMixin.py
+++ src/census/original_docs/OriginalDocPDFMixin.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from functools import cached_property
 
 from utils_future import WWW, File, Log
 
@@ -10,17 +11,15 @@ class OriginalDocPDFMixin:
     DIR_ORIGINAL_DATA = "original_data"
     DIR_ORIGINAL_DATA_PDFS = os.path.join(DIR_ORIGINAL_DATA, "pdfs")
 
+    @cached_property
     def pdf_file_path(self):
         return os.path.join(self.dir_data, "original.pdf")
 
     def download_pdf(self):
-        basename = os.path.basename(self.url)
-        pdf_path = os.path.join(self.DIR_ORIGINAL_DATA_PDFS, basename)
-        if os.path.exists(pdf_path):
-            shutil.copy(pdf_path, self.pdf_file_path())
-            log.debug(f"{File(pdf_path)} exists")
-            return pdf_path
+        if os.path.exists(self.pdf_file_path):
+            log.debug(f"{File(self.pdf_file_path)} exists")
+            return self.pdf_file_path
         www = WWW(self.url)
         os.makedirs(self.DIR_ORIGINAL_DATA_PDFS, exist_ok=True)
-        www.download_binary(pdf_path)
-        return pdf_path
+        www.download_binary(self.pdf_file_path)
+        return self.pdf_file_path
