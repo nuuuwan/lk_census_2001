2ad76fcc679eb1394f10b9fc63a310760e3ebe0c
nuuuwan <nuuuwan@gmail.com>
2026-06-22 12:01:44 +0530

    copy pdfs

--- src/census/original_docs/OriginalDocPDFMixin.py
+++ src/census/original_docs/OriginalDocPDFMixin.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from utils_future import WWW, File, Log
 
@@ -9,10 +10,14 @@ class OriginalDocPDFMixin:
     DIR_ORIGINAL_DATA = "original_data"
     DIR_ORIGINAL_DATA_PDFS = os.path.join(DIR_ORIGINAL_DATA, "pdfs")
 
+    def pdf_file_path(self):
+        return os.path.join(self.dir_data, "original.pdf")
+
     def download_pdf(self):
         basename = os.path.basename(self.url)
         pdf_path = os.path.join(self.DIR_ORIGINAL_DATA_PDFS, basename)
         if os.path.exists(pdf_path):
+            shutil.copy(pdf_path, self.pdf_file_path())
             log.debug(f"{File(pdf_path)} exists")
             return pdf_path
         www = WWW(self.url)
