1ea1a369f28282922b2aa72309fadd5140e45b46
nuuuwan <nuuuwan@gmail.com>
2026-06-23 09:47:02 +0530

    more updates to CUSTOM_HEADER_MAP

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -9,7 +9,7 @@ def main():
     # OriginalDoc.build_global_readme()
 
     docs = OriginalDoc.list()
-    for doc in docs[10:15]:
+    for doc in docs[15:20]:
         os.system(f"open -a firefox {doc.pdf_file_path}")
 
     os.system('say "Pipeline Done"')
